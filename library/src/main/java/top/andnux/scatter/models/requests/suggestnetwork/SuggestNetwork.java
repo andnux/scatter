d4347fe968f8d683c7a6ea5f53320c0dc6487d0f
zhangchunlin <zhangchunlin@fishotech.com>
2020-01-06 17:50:21 +0800

    增加网络加载框

---
+++ library/src/main/java/top/andnux/scatter/models/requests/suggestnetwork/SuggestNetwork.java
@@ -0,0 +1,19 @@
+package top.andnux.scatter.models.requests.suggestnetwork;
+
+import top.andnux.scatter.models.Network;
+
+/**
+ * created on 2020/1/6
+ */
+public class SuggestNetwork {
+
+    private Network network;
+
+    public Network getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(Network network) {
+        this.network = network;
+    }
+}
